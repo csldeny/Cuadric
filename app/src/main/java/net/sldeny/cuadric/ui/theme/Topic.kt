86df2523a8d9d05ef1a15d6e3ea799c04f20fd30
sldeny <Denis@DESKTOP-NIA0DRO>
2023-04-27 14:39:26 -0600

    Initial commit

---
+++ app/src/main/java/net/sldeny/cuadric/ui/theme/Topic.kt
@@ -0,0 +1,10 @@
+package net.sldeny.cuadric.ui.theme
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Topic(
+    @StringRes val name: Int,
+    val availableCourses: Int,
+    @DrawableRes val imageRes: Int
+)
